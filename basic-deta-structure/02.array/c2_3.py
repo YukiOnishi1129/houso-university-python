f21f4186fbce5cb48790dc337965da5e41c0ec02
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-13 00:28:00 +0900

    numpy

---
+++ basic-deta-structure/02.array/c2_3.py
@@ -0,0 +1,12 @@
+# NumPy
+# Numerical Python
+# ベクトルや行列などの数値計算を効率的に行うためのライブラリ
+# n次元配列を扱うためのクラスが提供されている
+# ndarray: (n-dimensional array) 多次元配列
+
+import numpy as np
+
+np_a = np.array([1, 2, 3, 4, 5])
+
+for i in np_a:
+    print(i, end = " ")
